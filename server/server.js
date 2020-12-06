195de6e163ef2c9ed2b6c8b3cdcbeb1902d15088
Do Huu Duy <duydo0802@gmail.com>
2020-12-06 16:56:03 +0700

    Test git

--- server/server.js
+++ server/server.js
@@ -4,12 +4,19 @@ const cors = require("cors");
 const server = require("http").Server(app);
 const router = require("./api");
 const bodyParser = require("body-parser");
+const { Socket } = require("socket.io");
 const io = require("socket.io")(server);
 require("./repositories/index");
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(router);
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+  });
+});
 
 server.listen(5050, () => {
   console.log("Server is running in 5050");
